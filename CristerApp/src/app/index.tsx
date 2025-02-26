0360bbaf65cc9a93d4a6fa8f3dc394f34aadbd6c
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-02-26 20:19:38 -0300

    update
    update

--- CristerApp/src/app/index.tsx
+++ CristerApp/src/app/index.tsx
@@ -1,8 +1,26 @@
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet, ImageBackground} from "react-native";
 export default  function Index(){
     return(
-        <View>
-            <Text>Hello Cristers</Text>
-        </View>
+        <ImageBackground 
+            source={require("../../assets/images/bgLogin.webp")} // Caminho da imagem
+            style={styles.background}>
+                <View>
+                    <Text>Hello Cristers</Text>
+                </View>
+            </ImageBackground>
+        
     )
 }
+const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      resizeMode: "cover", // Ajusta a imagem ao fundo
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)", // Fundo semitransparente
+      padding: 20,
+      borderRadius: 10,
+    },
+})
